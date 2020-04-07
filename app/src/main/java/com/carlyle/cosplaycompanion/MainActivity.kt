9d1b89ea7a7bec25e170146b8c5dea32c62999b7
BrennanTCarlyle <brennan.carlyle@gmail.com>
2020-04-06 20:21:41 -0900

    Added random number generator with button

--- app/src/main/java/com/carlyle/cosplaycompanion/MainActivity.kt
+++ app/src/main/java/com/carlyle/cosplaycompanion/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.carlyle.cosplaycompanion
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val myRandom = Random()
+        val buttonGenerate =
+            findViewById<View>(R.id.generate) as Button
+        val textGenerateNumber =
+            findViewById<View>(R.id.generatenumber) as TextView
+        buttonGenerate.setOnClickListener {
+            // TODO Auto-generated method stub
+            textGenerateNumber.text = myRandom.nextInt(100).toString()
+        }
     }
 }
