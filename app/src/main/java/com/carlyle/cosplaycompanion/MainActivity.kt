657fc3f8b2e96a10a1f413ed83e4c462ff941d0a
BrennanTCarlyle <brennan.carlyle@gmail.com>
2020-04-13 21:12:05 -0900

    Added nav bar & fixed button issues

--- app/src/main/java/com/carlyle/cosplaycompanion/MainActivity.kt
+++ app/src/main/java/com/carlyle/cosplaycompanion/MainActivity.kt
@@ -12,14 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myRandom = Random()
-        val buttonGenerate =
-            findViewById<View>(R.id.generate) as Button
-        val textGenerateNumber =
-            findViewById<View>(R.id.generatenumber) as TextView
-        buttonGenerate.setOnClickListener {
-            // TODO Auto-generated method stub
-            textGenerateNumber.text = myRandom.nextInt(100).toString()
-        }
+
+
     }
 }
