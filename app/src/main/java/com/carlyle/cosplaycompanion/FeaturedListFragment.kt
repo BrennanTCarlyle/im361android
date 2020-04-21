0d1ade047a1da971390d53986a2778d4bc95d5d0
BrennanTCarlyle <brennan.carlyle@gmail.com>
2020-04-20 22:48:22 -0900

    Added viewPager

---
+++ app/src/main/java/com/carlyle/cosplaycompanion/FeaturedListFragment.kt
@@ -0,0 +1,19 @@
+
+package com.carlyle.cosplaycompanion
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FeaturedListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstance: Bundle?
+
+    ): View? {
+        return inflater.inflate(R.layout.fragment_featured_list, container, false)
+    }
+}
--- app/src/main/java/com/carlyle/cosplaycompanion/MainActivity.kt
+++ app/src/main/java/com/carlyle/cosplaycompanion/MainActivity.kt
@@ -5,14 +5,33 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import java.util.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
+    lateinit var viewpager: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        viewpager = findViewById(R.id.view_pager)
 
+        val adapter = MainActivityPagerAdapter(this)
+        viewpager.adapter = adapter
+
+
+    }
+    private inner class MainActivityPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity){
+        override fun getItemCount(): Int{
+             return 3
+        }
+        override fun createFragment(position: Int): Fragment {
+            return FeaturedListFragment()
+        }
     }
 }
